62b5d7d3ef184270c10b06f4cd07091c4fb9f617
Romain J <romain.ordi@gmail.com>
2022-05-22 16:12:00 +0200

    update(routes): change ws route

--- chat/apps/guilds/features/channels/routing.py
+++ chat/apps/guilds/features/channels/routing.py
@@ -5,7 +5,8 @@ from .consumers import consumers
 
 websocket_urlpatterns = [
     re_path(
-        r"^(?P<guild_id>[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12})"
+        r"^ws"
+        r"/(?P<guild_id>[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12})"
         r"/(?P<channel_id>[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12})$",
         consumers.ChatConsumer.as_asgi(),
     ),
